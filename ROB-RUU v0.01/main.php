7b7b55c7fa310a51699b1dab6bad646a0351bd74
jarupong pajakgo <lagmanmc@gmail.com>
2017-01-27 14:40:33 +0700

    new design

---
+++ ROB-RUU v0.01/main.php
@@ -0,0 +1,145 @@
+<html><head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="http://pingendo.github.io/pingendo-bootstrap/themes/default/bootstrap.css" rel="stylesheet" type="text/css">
+    <style>
+      @font-face {font-family: thaisan;
+                  src: url(thaisanslite_r1.ttf);}
+      * {font-family: thaisan; !important;}}
+    </style>
+    <title>ง่วง</title>
+  </head><body style="background-color:ffe776">
+    <nav class="navbar navbar-default navbar-static-top" style="background-color:white">
+      <div class="navbar-header">
+        <ul class="nav navbar-nav">
+          <li>
+            <a class="navbar-brand" href="#"><img src="pic/brand.png" style="height: 60">
+                        </a>
+          </li>
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-ex-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </ul>
+      </div>
+      <div class="container-fluid">
+        <div class="collapse navbar-collapse" id="navbar-ex-collapse">
+          <div class="navbar-header">
+            <ul class="nav navbar-nav">
+              <li>
+                 <form class="navbar-form" style="margin-top:27px">
+                  <div class="input-group">
+                    <input type="text" class="form-control" placeholder="ค้นหาบทเรียน">
+                    <div class="input-group-btn">
+                      <button class="btn btn" type="button">
+                        <span class="glyphicon glyphicon-search"></span>
+                        <i class="icon-search"></i>
+                      </button>
+                    </div>
+                  </div>
+                </form>
+              </li>
+            </ul>
+          </div>
+          <ul class="nav navbar-nav navbar-right" align="center">
+            <li>
+              <a href="#"><font size="5"> 1000</font> <img src="icon/point2.png" style="height:60;width:41;margin-left:2"></a>
+            </li>
+            <li>
+              <a class="dropdown-toggle" data-toggle="dropdown" href="#"><img src="pic/user.png" style="width:60;height:60" class="img-circle">
+                            <span class="caret"></span>
+                        </a>
+              <ul class="dropdown-menu">
+                <li>
+                  <a href="#"><h4>Profile</h4></a>
+                </li>
+                <li>
+                  <a href="#"><h4>My note</h4></a>
+                </li>
+                <li>
+                  <a href="#"><h4>Donate</h4></a>
+                </li>
+                <li>
+                  <a href="#"><h4>Logout</h4></a>
+                </li>
+              </ul>
+            </li>
+            <li class="hidden-sm hidden-xs">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12" style="background-color:white">
+          <div class="carousel slide" id="fullcarousel-example" data-interval="false" data-ride="carousel">
+            <div class="carousel-inner">
+              <div class="item active">
+                <img src="pic/อิ-อิ.png">
+                <div class="carousel-caption">
+                  <h2>Title</h2>
+                  <p>Description</p>
+                </div>
+              </div>
+            </div>
+            <a class="left carousel-control" href="#fullcarousel-example" data-slide="prev"><i class="icon-prev fa fa-angle-left"></i></a>
+            <a class="right carousel-control" href="#fullcarousel-example" data-slide="next"><i class="icon-next fa fa-angle-right"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container" style="background-color:white">
+      <div class="row">
+        <div class="col-md-3">
+          <center>
+            <img src="button/01study.png" class="img-responsive">
+          </center>
+        </div>
+        <div class="col-md-3">
+          <center>
+            <img src="button/02teach.png " class="img-responsive">
+          </center>
+        </div>
+        <div class="col-md-3">
+          <center>
+            <img src="button/03board.png" class="img-responsive">
+          </center>
+        </div>
+        <div class="col-md-3">
+          <center>
+            <img src="button/04chat.png" class="img-responsive">
+          </center>
+        </div>
+      </div>
+    </div>
+    <div class="container" style="background-color:white">
+      <div class="row"></div>
+    </div>
+    <div class="container" style="background-color:white">
+      <div class="row">
+        <div class="col-md-6"></div>
+        <div class="col-md-6">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-6"></div>
+            </div>
+            <div class="row">
+              <div class="col-md-6"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  
+
+</body></html>
